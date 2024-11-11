32577f8a5d79e37b1b3529fcac4777a5283dd290
searica <143636061+searica@users.noreply.github.com>
2024-11-11 09:58:55 -0800

    Use GUID instead of name
    This is why GUID was added to ModModule

--- JotunnLib/Utils/ModCompatibility/ModCompatibility.cs
+++ JotunnLib/Utils/ModCompatibility/ModCompatibility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -52,7 +52,7 @@ namespace Jotunn.Utils
                     return false;
                 }
                 var serverData = new ModuleVersionData(LastServerVersion);
-                var moduleNames = new HashSet<string>(serverData.Modules.Select(mod => mod.name).ToList());
+                var moduleNames = new HashSet<string>(serverData.Modules.Select(mod => mod.guid).ToList());
                 return moduleNames.Contains(modGUID);
             }
             return true;
