dd3ae5a8479faf97d3e9cf597831d1d594c8e30c
searica <143636061+searica@users.noreply.github.com>
2024-11-13 12:01:31 -0800

    Only select GUID if not null

--- JotunnLib/Utils/ModCompatibility/ServerVersionData.cs
+++ JotunnLib/Utils/ModCompatibility/ServerVersionData.cs
@@ -24,13 +24,13 @@ namespace Jotunn.Utils
         internal ServerVersionData(List<ModModule> versionData)
         {
             moduleVersionData = new ModuleVersionData(versionData);
-            moduleGUIDs = new HashSet<string>(moduleVersionData.Modules.Select(mod => mod.guid).ToList());
+            moduleGUIDs = new HashSet<string>(moduleVersionData.Modules.Where(x => x.guid != null).Select(x => x.guid).ToList());
         }
 
         internal ServerVersionData(System.Version valheimVersion, List<ModModule> versionData)
         {
             moduleVersionData = new ModuleVersionData(valheimVersion, versionData);
-            moduleGUIDs = new HashSet<string>(moduleVersionData.Modules.Select(mod => mod.guid).ToList());
+            moduleGUIDs = new HashSet<string>(moduleVersionData.Modules.Where(x => x.guid != null).Select(x => x.guid).ToList());
         }
 
         /// <summary>
@@ -40,7 +40,7 @@ namespace Jotunn.Utils
         internal ServerVersionData(ZPackage pkg)
         {
             moduleVersionData = new ModuleVersionData(pkg);
-            moduleGUIDs = new HashSet<string>(moduleVersionData.Modules.Select(mod => mod.guid).ToList());
+            moduleGUIDs = new HashSet<string>(moduleVersionData.Modules.Where(x => x.guid != null).Select(x => x.guid).ToList());
         }
 
         internal bool IsValid()
